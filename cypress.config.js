5271c8d1ca6578c0dd6a2de5377a0ada260d0493
Vladislav Sukhin <vladislav@kubeshop.io>
2022-09-07 20:25:05 +0300

    add support file

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   component: {
-    specPattern: 'src/**/*.cy.js'
+    specPattern: 'src/**/*.cy.js',
+    supportFile: false
   },
   e2e: {
-    specPattern: 'cypress/integration/**/*.cy.js'
+    specPattern: 'cypress/integration/**/*.cy.js',
+    supportFile: false
   },
 })
